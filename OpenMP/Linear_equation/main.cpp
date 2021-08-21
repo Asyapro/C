6d9b05038a665b46325ba09bcff81f1aa510542c
Asyapro <52502005+Asyapro@users.noreply.github.com>
2021-08-21 17:49:20 +0300

    Create main.cpp

---
+++ OpenMP/Linear_equation/main.cpp
@@ -0,0 +1,23 @@
+
+#include "Test.cpp"
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	Test test;
+	test.toString(); //все параметры задаются через консоль, результаты операций выводятся на экран
+
+	int amount_test = 1000;
+	int n = 50;
+	//cout << "\nenter size for time-test\n";
+	//cin >> n;
+	//cout << "\nenter amount of time-test\n";
+	// cin >> amount_test;
+
+	//cout << "\ncomputation time-test\n";
+	//test.test_time(amount_test, n); //проведение тестов на время работы
+	//test.get_results();
+}
