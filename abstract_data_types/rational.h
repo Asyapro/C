6fc0055147e2fa2323e6f4066c91d6cdb84a66a1
Asyapro <52502005+Asyapro@users.noreply.github.com>
2021-08-21 17:56:27 +0300

    Create rational.h

---
+++ abstract_data_types/rational.h
@@ -0,0 +1,55 @@
+#include <iosfwd>
+
+class Rational {
+ public:
+    Rational() {}
+    explicit Rational(const int numerator);
+    Rational(const int numerator, const int denominator);
+    bool operator==(const Rational& rhs) const { return (num * rhs.den == rhs.den * num); }
+    bool operator!=(const Rational& rhs) const { return !operator==(rhs); }
+    bool operator>(const Rational& rhs) const { return (num * rhs.den - rhs.den * num > 0); }
+    bool operator<(const Rational& rhs) const { return (num * rhs.den - rhs.den * num < 0); }
+    bool operator>=(const Rational& rhs) const { return (num * rhs.den - rhs.den * num >= 0); }
+    bool operator<=(const Rational& rhs) const { return (num * rhs.den - rhs.den * num <= 0); }
+    Rational& operator=(const Rational& rhs) = default;
+    Rational& operator+=(const Rational& rhs);
+    Rational& operator+=(const int rhs) { return operator+=(Rational(rhs)); }
+    Rational& operator-=(const Rational& rhs);
+    Rational& operator-=(const int rhs) { return operator-=(Rational(rhs)); }
+    Rational& operator*=(const Rational& rhs);
+    Rational& operator*=(const int rhs) { return operator*=(Rational(rhs)); }
+    Rational& operator/=(const Rational& rhs);
+    Rational& operator/=(const int rhs) { return operator/=(Rational(rhs)); }
+
+    void normal();
+
+    std::ostream& writeTo(std::ostream& ostrm);
+    std::istream& readFrom(std::istream& istrm);
+
+    static const char seperator{ '/' };
+
+    int num{ 0 };
+    int den{ 1 };
+};
+
+Rational operator+(const Rational& lhs, const Rational& rhs);
+Rational operator-(const Rational& lhs, const Rational& rhs);
+Rational operator*(const Rational& lhs, const Rational& rhs);
+Rational operator/(const Rational& lhs, const Rational& rhs);
+
+Rational operator+(const Rational& lhs, const int& rhs);
+Rational operator-(const Rational& lhs, const int& rhs);
+Rational operator*(const Rational& lhs, const int& rhs);
+Rational operator/(const Rational& lhs, const int& rhs);
+
+Rational operator+(const int& lhs, const  Rational& rhs);
+Rational operator-(const int& lhs, const Rational& rhs);
+Rational operator*(const int& lhs, const Rational& rhs);
+Rational operator/(const int& lhs, const Rational& rhs);
+
+inline std::ostream& operator<<(std::ostream& ostrm, Rational& rhs) {
+    return rhs.writeTo(ostrm);
+}
+inline std::istream& operator>>(std::istream& istrm, Rational& rhs) {
+    return rhs.readFrom(istrm);
+}
